08b79e22f6fe4e324b9368583e43eb128da8aea5
LuCplusplus <landsmanoval@gmail.com>
2022-11-12 18:01:42 +0100

    TodoScreen

--- app/src/main/java/com/example/todoapp/ui/todolist/TodoListScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/todolist/TodoListScreen.kt
@@ -1,11 +1,22 @@
 package com.example.todoapp.ui.todolist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TodoListScreen() {
     // 1. Add button for going to AddTodoScreen
     // 2. Implement AddTodoScreen
     // 3. Implement todolist
+
+
 }
 
